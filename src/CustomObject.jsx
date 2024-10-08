afa50a577756f65542d126709dedb31a028b927d
jawid-fakhari <jawid_fakhari@live.it>
2024-10-08 12:43:13 +0200

    Optimise vertices with useMemo

--- src/CustomObject.jsx
+++ src/CustomObject.jsx
@@ -1,14 +1,24 @@
+import { useMemo } from 'react'
 import * as THREE from 'three'
 
 //Come creare un costum geometry
 //Piu o meno uguale al native three.js
 function CustomObject() {
-    const verticesCount = 10 * 3
-    const positions = new Float32Array(verticesCount * 3)
 
-    for (let i = 0; i < verticesCount * 3; i++) {
-        positions[i] = (Math.random() - 0.5) * 3
-    }
+    const verticesCount = 10 * 3
+
+    //useMemo: mandiamo una func al useMemo, lui lo chiama e ricorda il valore, e se il componente viene renderizzato di nuovo allora useMemo ritorna il valore gia preso dalla prima chiamata di func
+    const positions = useMemo(() => {
+
+        const positions = new Float32Array(verticesCount * 3)
+
+        for (let i = 0; i < verticesCount * 3; i++) {
+            positions[i] = (Math.random() - 0.5) * 3
+        }
+
+        return positions
+    }, [])
+    
 
     return (
         <>
@@ -21,7 +31,7 @@ function CustomObject() {
                         array={positions}
                     />
                 </bufferGeometry>
-                <meshBasicMaterial color="orange" side={THREE.DoubleSide}/>
+                <meshBasicMaterial color="orange" side={THREE.DoubleSide} />
             </mesh>
         </>
     )
