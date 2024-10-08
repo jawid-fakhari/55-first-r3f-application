22a4766d86aa4ea2dcad44fe254056a562804705
jawid-fakhari <jawid_fakhari@live.it>
2024-10-08 10:45:58 +0200

    how to animate cube, need useRef, how to import delta time in useFrame callback func

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,22 @@
 import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
 
 function Experience() {
-  //in r3f, la scena renderizzato on each frame ma niente si muove
-  //quindi usiamo useFrame che è r3f hook
-  useFrame(() => {
-    console.log('tick');
+
+  //usiamo useRef hook a referenziare il valore del mesh del cubo
+  const cubeRef = useRef()
+
+  //useFrame prende il valore corrente del rotazione sul asse y e aggiunge 0.01
+  //nel useFrame basta passare due parameteri state e delta, e quindi non c'è bisogno di fare il calcolo di delta. state ci ritorna lo stato dello mesh
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta * 0.5
   })
 
   return (
     <>
-      <mesh rotation-y={Math.PI * 0.23} position={[2, 0, 0]} scale={1.5}>
+      <mesh ref={cubeRef} rotation-y={Math.PI * 0.23} position={[2, 0, 0]} scale={1.5}>
         <boxGeometry />
-        <meshBasicMaterial color="mediumPurple" wireframe="false" />
+        <meshBasicMaterial color="mediumPurple" wireframe />
       </mesh>
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
