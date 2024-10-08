f627f8ca5720d5d0cb8c352876005a51ba0e248e
jawid-fakhari <jawid_fakhari@live.it>
2024-10-08 09:35:34 +0200

    how to pass properties of a geometry via "args" in r3f

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,8 +4,8 @@ function Experience() {
   return (
     <>
       <mesh>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
+        <sphereGeometry args={[1.5, 32, 32]} />
+        <meshBasicMaterial />
       </mesh>
     </>
   )
