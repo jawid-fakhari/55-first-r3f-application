eeff5270c1ff7efbab24f7002c36def8fba1ed5f
jawid-fakhari <jawid_fakhari@live.it>
2024-10-08 09:29:21 +0200

    first component in r3f

---
+++ src/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Experience() {
+  return (
+    <>
+      <mesh>
+        <torusKnotGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </>
+  )
+}
+
+export default Experience
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,12 @@
 import { Canvas } from '@react-three/fiber'
 import './style.css'
 import ReactDOM from 'react-dom/client'
+import Experience from './Experience'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <>
-        <Canvas>
-            <mesh>
-                <torusKnotGeometry />
-                <meshNormalMaterial />
-            </mesh>
-        </Canvas>
-    </>
+    <Canvas>
+        <Experience />
+    </Canvas>
 )
