eff63e09e7146c5e58b7f377bda92cdcfca16e75
jawid-fakhari <jawid_fakhari@live.it>
2024-10-08 09:46:54 +0200

    another way to add args in a tag

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Wireframe } from 'three/examples/jsm/Addons.js'
 
 function Experience() {
   return (
     <>
       <mesh>
         <sphereGeometry args={[1.5, 32, 32]} />
-        <meshBasicMaterial />
+        <meshBasicMaterial color= "purple" wireframe />
       </mesh>
     </>
   )
