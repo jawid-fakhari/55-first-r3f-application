ea55fe74cd5cb624859e8deb5caa460844b25f13
jawid-fakhari <jawid_fakhari@live.it>
2024-10-08 11:42:31 +0200

    how to apply ORBITCONTROLS in oure scene

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,17 @@
-import { useFrame } from "@react-three/fiber"
+import { extend, useFrame, useThree } from "@react-three/fiber"
 import { useRef } from "react"
+import { OrbitControls } from "three/examples/jsm/Addons.js"
+
+//extend method, converte una classe Three.js a una versione dichiarativ e la rende disponibile in jsx
+//chiamiamo extend sulla OrbitControls, cosi possiamo usare <orbitControls> all'interno di jsx
+extend({ OrbitControls })
 
 function Experience() {
+
+  //useThree hook ci da lo stato del canvas "come nel useFrame((state, delta))" ma qui solo una volta che tutto è pronto
+  //useThree contiene tutto cio che abbiamo bisogno come camera, WebGLRenderer, clock possiamo usare three.CioCheVogliamo oppure usare distructure e prendere solo cio che ci serve
+  const { camera, gl } = useThree()
+
 
   //usiamo useRef hook a referenziare il valore del mesh del cubo
   const groupRef = useRef()
@@ -10,12 +20,13 @@ function Experience() {
   //useFrame prende il valore corrente del rotazione sul asse y e aggiunge 0.01
   //nel useFrame basta passare due parameteri state e delta, e quindi non c'è bisogno di fare il calcolo di delta. state ci ritorna lo stato dello mesh
   useFrame((state, delta) => {
-    groupRef.current.rotation.y += delta * 0.5
+    // groupRef.current.rotation.y += delta * 0.5
     cubeRef.current.rotation.y += delta * 0.5
   })
 
   return (
     <>
+      <orbitControls args={[camera, gl.domElement]} />
       <group ref={groupRef}>
         <mesh position-x={-2}>
           <sphereGeometry args={[1, 8, 1]} />
