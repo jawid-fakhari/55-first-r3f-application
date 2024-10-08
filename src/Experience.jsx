790ee214ffa731d4f197143adf287b8abcc85fca
jawid-fakhari <jawid_fakhari@live.it>
2024-10-08 10:17:52 +0200

    practice

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { PlaneGeometry } from 'three'
 import { Wireframe } from 'three/examples/jsm/Addons.js'
 
 function Experience() {
   return (
     <>
-      <mesh rotation-y={Math.PI * 0.23} position={[2,0,0]} scale={1.5}>
-        {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
-        <boxGeometry/>
-        <meshBasicMaterial color="purple" wireframe />
+      <mesh rotation-y={Math.PI * 0.23} position={[2, 0, 0]} scale={1.5}>
+        <boxGeometry />
+        <meshBasicMaterial color="mediumPurple" wireframe="false" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry/>
+        <meshBasicMaterial color="greenyellow"/>
       </mesh>
     </>
   )
